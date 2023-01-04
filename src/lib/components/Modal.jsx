2ecf5be1c4b3a6a097ba73597eaa6ba89247a6ae
LauNM <laurie.beduneau@gmail.com>
2023-01-04 17:25:02 +0100

    added test

--- src/lib/components/Modal.jsx
+++ src/lib/components/Modal.jsx
@@ -34,13 +34,13 @@ function Modal  (
   const footerStyle = footer ? { backgroundColor: footerBackgroundColor, borderTop: borderDivider } : {};
 
   return (
-    <div className={ `modal-wrapper ${ displayModal } ${ modalSize }` }>
+    <div data-testid="modal-container" className={ `modal-wrapper ${ displayModal } ${ modalSize }` }>
       <div className="modal" style={{ backgroundColor: modalBackground }}>
-        {hasCloseIcon && <span onClick={closeModal} className="close-button">
+        {hasCloseIcon && <span data-testid="close-button" onClick={closeModal} className="close-button">
           {closeIcon ?? <CloseIconDefault /> }
       </span> }
        <header style={ headerStyle }>{ header }</header>
-        { props.children && <div className="modal-main">
+        { props.children && <div data-testid="modal-main" className="modal-main">
           { props.children }
         </div>
         }
